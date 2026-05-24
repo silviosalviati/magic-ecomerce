234543584d22c2a92d127cb078fc038578a240b4
silviosalviati <silviosalviati@gmail.com>
2026-05-24 15:55:33 -0300

    Fix frontend production build import issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { CartSidebar } from './components/CartSidebar';
 import { Footer } from './components/Footer';
